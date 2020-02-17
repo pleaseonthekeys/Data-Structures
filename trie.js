94547145ed106c79d3cc245b0b20f0c0a1808eb6
Lauren Albert <pleaseonthekeys@gmail.com>
2020-02-17 14:41:27 -0500

    code clean up, findSuggestions method initialized

--- trie.js
+++ trie.js
@@ -29,79 +29,21 @@ Trie.prototype.addWord = function(word) {
   }
 };
 
-//traverse/access/touch
-//do all of the steps in my head THEN: identify pattern: do this pattern in a loop OR a recursive call
 Node.prototype.traverse = function(func = console.log) {
-  func(this.letter);
+  func(this);
   Object.values(this.children).forEach(child => child.traverse());
 };
 
 Trie.prototype.traverse = function(func = console.log) {
-  this.root.traverse(func);
-};
-
-Trie.prototype.contains = function(searchTerm) {
-  //try using traverse here
-};
-
-Trie.prototype.getOneWord = function(letter) {
-  let word = '';
-  let node = this.root;
-  while (node !== undefined) {
-    if (node.complete) {
-      return word;
-    }
-    word += node.letter;
-    node = node.children[letter];
-    console.log(node);
-  }
-  console.log(word);
+  this.root.traverse();
 };
 
 Trie.prototype.findAllWords = function() {
-  // //return an array with complete words
-  // let allWords = {};
-  // //each property of allWords will be the root node's children
-  // let fullWord = '';
-  // let currentNode = this.root;
-  // Object.keys(this.root.children).forEach(child => {
-  //   allWords[child] = [];
-  // });
-  // //1. push the current node's letter into a fullWord string
-  // function makeWord(currentNode, letter) {
-  //   Object.entries(currentNode).forEach(node => {
-  //     console.log(node[0]);
-  //     fullWord += node[0];
-  //     if (node[1].complete) {
-  //       allWords[letter].push(fullWord);
-  //       fullWord = '';
-  //     } else if (node[1].children) {
-  //       console.log(currentNode);
-  //       makeWord(node[1].children, 't');
-  //     }
-  //   });
-  // }
-  // for (let char in allWords) {
-  //   // console.log({
-  //   //   current: currentNode.children,
-  //   //   char: char,
-  //   //   checking: currentNode.children[char]
-  //   // });
-  //   makeWord(currentNode.children, char);
-  // }
-  // console.log(allWords);
-  // let letterVal = current[char];
-  //2. check the node's complete property
-  //a word is complete if it has a 'complete' property equal to true
-  //3. if complete is true, push that word into that property's result array
-  //4. if that node also has children, repeat this process
-  //5. if it does not have children, move on to the next property in allWords(root node's next child)
-  //6. if complete is false, move on to that node's children and repeat these steps
   let words = [];
   let search = function(node, string) {
     if (node.children) {
       for (let letter in node.children) {
-        search(node.children[letter], string.concat(letter));
+        search(node.children[letter], string + letter);
       }
       if (node.complete) {
         words.push(string);
@@ -109,13 +51,25 @@ Trie.prototype.findAllWords = function() {
     }
   };
   search(this.root, '');
-  console.log({ words });
   return words.length > 0 ? words : null;
 };
 
-Trie.prototype.findSuggestions = function(/*character or part of a word */) {
+Trie.prototype.findSuggestions = function(segment) {
   //return all suggestions for that input
   let suggestions = {};
+  //findAllWords returns an array
+  let wordArr = this.findAllWords();
+  wordArr.forEach(word => {
+    let firstChar = word[0];
+    if (suggestions[firstChar]) {
+      suggestions[firstChar].push(word);
+    } else {
+      suggestions[firstChar] = [word];
+    }
+  });
+  console.log(suggestions);
+  //should I import that array, sort it, and derive suggestions from there?
+  //should I write a method from scratch without using findAll?
 };
 
 let searchFunctionality = new Trie();
@@ -125,9 +79,8 @@ searchFunctionality.addWord('app');
 searchFunctionality.addWord('car');
 searchFunctionality.addWord('train');
 searchFunctionality.addWord('travel');
-searchFunctionality.getOneWord('a');
-console.log(searchFunctionality.findAllWords());
-// searchFunctionality.traverse();
+searchFunctionality.findAllWords();
+searchFunctionality.findSuggestions('a');
 
 let letterArr = ['a,b', 'b,c', 'c,d', 'a,e'];
 function makeNewTrieforJosh(arr) {
@@ -136,5 +89,7 @@ function makeNewTrieforJosh(arr) {
   newArr.forEach(val => testing.addWord(val));
   return testing;
 }
-// console.log(makeNewTrieforJosh(letterArr));
-// console.log(searchFunctionality.findAllWords());
+
+let newTrie = makeNewTrieforJosh(letterArr);
+// console.log(newTrie);
+// console.log(newTrie.findAllWords());
